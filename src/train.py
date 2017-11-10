ba4baeca6e11c8ff3f5d461ac1ccfbea2135d7c5
Unknown <linzuolee@gmail.com>
2017-11-10 15:40:46 -0800

    Used auc score instead of F1 in cross validation

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ from sklearn import linear_model,tree
 from sklearn.model_selection import KFold
 from sklearn.metrics import f1_score
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score
 from features import getFeatureVecotrs
 
 
@@ -23,43 +24,43 @@ def main():
     for train_index, test_index in kf.split(x):
         x_train, x_test = x[train_index], x[test_index]
         y_train, y_test = y[train_index], y[test_index]
-        print('##############Training with Linear Regression##############')
-        reg = linear_model.LinearRegression()
+        print('##############Training with Logistic Regression##############')
+        reg = linear_model.LogisticRegression()
         reg.fit(x_train, y_train)
-        y_res = reg.predict(x_test)
-        f1_linear_reg.append(compute_f1(y_test, (y_res > 0.5).astype(int), 'Linear Regression'))
+        y_res = reg.predict_proba(x_test)
+        f1_linear_reg.append(aucScore(y_test, y_res, 'Logistic Regression'))
         print('##############Training with Random Forest##############')
         rf_clf = RandomForestClassifier(max_depth=2, random_state=0)
         rf_clf.fit(x_train, y_train)
-        y_res = rf_clf.predict(x_test)
-        f1_rf.append(compute_f1(y_test, y_res, 'Random Forest'))
+        y_res = rf_clf.predict_proba(x_test)
+        f1_rf.append(aucScore(y_test, y_res, 'Random Forest'))
 
         print('##############Training with Decision Tree ##############')
         tree_clf = tree.DecisionTreeClassifier()
         tree_clf.fit(x_train, y_train)
-        y_res = tree_clf.predict(x_test)
-        f1_tree.append(compute_f1(y_test, y_res, 'Decision Tree'))
+        y_res = tree_clf.predict_proba(x_test)
+        f1_tree.append(aucScore(y_test, y_res, 'Decision Tree'))
 
         print('##############Training with SGD ##############')
-        sgd_clf = linear_model.SGDClassifier(loss="hinge", penalty="l2")
+        sgd_clf = linear_model.SGDClassifier(loss="log", penalty="l2")
         sgd_clf.fit(x_train, y_train)
-        y_res = sgd_clf.predict(x_test)
-        f1_sgd.append(compute_f1(y_test, (y_res > 0.5).astype(int), 'Stochastic Gradient Descent'))
+        y_res = sgd_clf.predict_proba(x_test)
+        f1_sgd.append(aucScore(y_test, y_res, 'Stochastic Gradient Descent'))
 
     avg_linear = sum(f1_linear_reg)/4.0
     avg_rf = sum(f1_rf)/4.0
     avg_sgd = sum(f1_sgd)/4.0
     avg_tree = sum(f1_tree)/4.0
-    print('The average f1 score for linear regression is : %f' % avg_linear)
-    print('The average f1 score for random forest is : %f' % avg_rf)
-    print('The average f1 score for sgd is : %f' % avg_sgd)
-    print('The average f1 score for decision tree is : %f' % avg_tree)
+    print('The auc score for linear regression is : %f' % avg_linear)
+    print('The auc score for random forest is : %f' % avg_rf)
+    print('The auc score for sgd is : %f' % avg_sgd)
+    print('The auc score for decision tree is : %f' % avg_tree)
 
-def compute_f1(y_test, y_res, model_name):
-    print('The f1 score for' + model_name)
-    f1 = f1_score(y_test, y_res,)
-    print('Average f1: {0:0.2f}'.format(f1))
-    return f1
+def aucScore(y_test, y_res, model_name):
+    print('The auc score for' + model_name)
+    roc_acu = roc_auc_score(y_test, y_res[:, 1])
+    print('Average auc score: {0:0.2f}'.format(roc_acu))
+    return roc_acu
 
 
 if __name__ == "__main__": main()
