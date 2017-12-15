a6707c943b3596ed61ec7a5635179b66368e2332
Unknown <linzuolee@gmail.com>
2017-12-15 10:15:51 +0800

    0.676 with 590 iterations

--- src/lgbm_train.py
+++ src/lgbm_train.py
@@ -32,10 +32,59 @@ members = pd.read_csv(path + 'members.csv', dtype={'city': 'category',
                                                    'registered_via': 'category'})
 
 songs_extra = pd.read_csv(path + 'song_extra_info.csv')
+print('song features')
+# filling missing values
+def is_chinese(x):
+    if '3.0' in str(x) or '10.0' in str(x) or '24.0' in str(x) or '59.0' in str(x):
+        return 1
+    return 0
+
+def genre_id_count(x):
+    if x == 'no_genre_id':
+        return 0
+    else:
+        return x.count('|') + 1
+
+def lyricist_count(x):
+    if x == 'no_lyricist':
+        return 0
+    else:
+        return sum(map(x.count, ['|', '/', '\\', ';'])) + 1
+    return sum(map(x.count, ['|', '/', '\\', ';']))
+
+def composer_count(x):
+    if x == 'no_composer':
+        return 0
+    else:
+        return sum(map(x.count, ['|', '/', '\\', ';'])) + 1
+
+def is_featured(x):
+    if 'feat' in str(x) :
+        return 1
+    return 0
+
+songs['lyricist'] = songs['lyricist'].cat.add_categories(['no_lyricist'])
+songs['lyricist'].fillna('no_lyricist',inplace=True)
+songs['genre_ids'] = songs['genre_ids'].cat.add_categories(['no_genre_id'])
+songs['genre_ids'].fillna('no_genre_id',inplace=True)
+songs['composer'] = songs['composer'].cat.add_categories(['no_composer'])
+songs['composer'].fillna('no_composer',inplace=True)
+songs.song_length.fillna(200000,inplace=True)
+# songs['artist_name'] = songs['artist_name'].cat.add_categories(['no_artist'])
+# songs['artist_name'].songs('no_artist',inplace=True)
+songs['is_chinese'] = songs['language'].apply(is_chinese).astype(np.uint8)
+songs['num_genre_ids'] = songs['genre_ids'].apply(genre_id_count).astype(np.uint8)
+songs['genre_ids'] = songs['genre_ids'].apply(lambda genre: str(genre).split('|')[0]).astype('category')
+songs['composer_count'] = songs['composer'].apply(composer_count).astype(np.uint8)
+songs['lyricist_count'] = songs['lyricist'].apply(lyricist_count).astype(np.uint8)
+songs['is_featured'] = songs['artist_name'].apply(is_featured).astype(np.uint8)
+print('\t Done')
+
 
 print('...Data preprocessing...')
 # get rid of | in genre id
 songs['genre_ids'] = songs['genre_ids'].apply(lambda genre: str(genre).split('|')[0]).astype('category')
+
 #songs['song_length'] = (songs['song_length'] / 60000).astype(int)
 print('Generating members features...')
 members['registration_year'] = members['registration_init_time'].apply(lambda x: int(str(x)[0:4]))
@@ -56,9 +105,8 @@ members.loc[members.membership_length < 0, 'membership_length'] = 0
 #norm_range = members['membership_length'].max() - members['membership_length'].min()
 # normalize length to [0,1]
 # members['membership_length'] = (members['membership_length'] - members['membership_length'].min()) / norm_range
-mean_age = members['bd'].mean()
-members.loc[members.bd == 0, 'bd'] = mean_age
-
+# mean_age = members['bd'].mean()
+# members.loc[members.bd == 0, 'bd'] = mean_age
 
 def isrc_to_year(isrc):
     if type(isrc) == str:
@@ -73,13 +121,15 @@ def isrc_to_year(isrc):
 songs_extra['song_year'] = songs_extra['isrc'].apply(isrc_to_year)
 songs_extra.drop(['isrc', 'name'], axis=1, inplace=True)
 
+
+
 def get_features(data, train=True):
     """features to used"""
     featurs = [
         'source_system_tab', 'source_screen_name', 'source_type', 'song_length', 'genre_ids', 'language',
         'city', 'registered_via', 'registration_year', 'registration_month', 'registration_day',
         'expiration_year', 'expiration_month', 'expiration_day', 'song_year', 'membership_length',
-        'bd', 'gender',
+        'bd', 'gender', 'song_id_count', 'genre_id_count', 'artist_name_count', 'num_genre_ids', 'composer_count', 'lyricist_count', 'is_featured'
     ]
     label = []
     feature_vectors = pd.DataFrame()
@@ -91,6 +141,12 @@ def get_features(data, train=True):
     feature_vectors = feature_vectors.merge(members, on='msno', how='left')
     feature_vectors = feature_vectors.merge(songs, on='song_id', how='left')
     feature_vectors = feature_vectors.merge(songs_extra, on='song_id', how='left')
+    s = feature_vectors['song_id'].value_counts()
+    feature_vectors['song_id_count'] =feature_vectors['song_id'].apply(lambda x: s[x])
+    g = feature_vectors['genre_ids'].value_counts()
+    feature_vectors['genre_id_count'] =feature_vectors['genre_ids'].apply(lambda x: g[x])
+    a = feature_vectors['artist_name'].value_counts()
+    feature_vectors['artist_name_count'] =feature_vectors['artist_name'].apply(lambda x: a[x])
     if train:
         label = data['target']
     return feature_vectors[featurs], label
@@ -111,13 +167,14 @@ test_x, empty = get_features(test, train=False)
 train_x = train_x.apply(lambda x: x.fillna(x.value_counts().index[0]))
 test_x = test_x.apply(lambda x: x.fillna(x.value_counts().index[0]))
 print('Building data set...')
-train_data = lgb.Dataset(train_x,
+train_data = lgb.Dataset(train_x, 
                          label=labels,
                          categorical_feature=['source_system_tab', 'source_screen_name', 'source_type', 'language'])
 valid_data = lgb.Dataset(train_x, labels)
 
 print('Training......')
-bst = lgb.train(params, train_data, num_round, valid_sets=valid_data, verbose_eval=5)
+#bst = lgb.train(params, train_data, num_round, valid_sets=valid_data, verbose_eval=5)
+bst = lgb.train(params, train_data, 590, valid_sets=valid_data, verbose_eval=10,) 
 
 print('Predicting......')
 pred = bst.predict(test_x)
@@ -126,4 +183,5 @@ print('##############Writing output##############')
 sub = pd.DataFrame()
 sub['id'] = test['id']
 sub['target'] = pred
-sub.to_csv('out.csv', index=False, mode='w+')
+#sub.to_csv('out.csv', index=False, mode='w+')
+sub.to_csv('lgbm_submission.csv.gz', compression = 'gzip', index=False, float_format = '%.5f') 
